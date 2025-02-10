5c5b31ec136e6d7c40b49965772f459e466dc3bf
dev-victor-braz <victor.90braz@gmail.com>
2025-02-11 00:54:40 +0100

    update tests, view, service namings

--- InvoicesAccounting/app/models/invoice_model.py
+++ InvoicesAccounting/app/models/invoice_model.py
@@ -3,12 +3,19 @@ from InvoicesAccounting.app.enums.invoice_states import InvoiceStates
 from django.core.exceptions import ValidationError
 
 class InvoiceModel(models.Model):
+
     provider = models.CharField(max_length=255)
+
     concept = models.TextField()
+
     base_value = models.DecimalField(max_digits=10, decimal_places=2)
+
     vat = models.DecimalField(max_digits=10, decimal_places=2)
+
     total_value = models.DecimalField(max_digits=10, decimal_places=2)
+
     date = models.DateField()
+    
     state = models.CharField(
         max_length=20,
         choices=[(state.value, state.name.capitalize()) for state in InvoiceStates],  
@@ -16,7 +23,7 @@ class InvoiceModel(models.Model):
     )
 
     def clean(self):
-        """ Validación de reglas de negocio antes de guardar el modelo """
+
         if self.base_value <= 0:
             raise ValidationError({"base_value": "Base value must be greater than zero."})
 

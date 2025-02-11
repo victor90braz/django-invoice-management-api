a8635ab2f91e416d256bdca56158fbb866211963
dev-victor-braz <victor.90braz@gmail.com>
2025-02-11 17:43:58 +0100

    add api_config.py

--- Inmatic/urls.py
+++ Inmatic/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from .config.api_config import SWAGGER_SCHEMA_CONFIG 
 
 from InvoicesAccounting.resources.views.invoice_view import (
     list_invoices,
@@ -13,14 +13,7 @@ from InvoicesAccounting.resources.views.invoice_view import (
 )
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="Invoices API",
-        default_version='v1',
-        description="API for managing invoices",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="victor.90braz@gmail.com"),
-        license=openapi.License(name="BSD License"),
-    ),
+    SWAGGER_SCHEMA_CONFIG,
     public=True,
 )
 
